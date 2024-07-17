35c84a2b81ca0aaf3ec9423ba60dbb78e475eb0e
awesomness560 <appsforyou6@gmail.com>
2024-07-17 16:09:21 -0500

    Reverting

--- Scripts/Multiplayer/MultiplayerSpawner.gd
+++ Scripts/Multiplayer/MultiplayerSpawner.gd
@@ -5,15 +5,20 @@ extends MultiplayerSpawner
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawn_function = spawnPlayer
-	SignalBus.joinedLobby.connect(joinedLobby)
-
-var players := {}
-
-func joinedLobby():##Spawns player whenver you join a lobby
-	if multiplayer.is_server():
+	if is_multiplayer_authority():
 		spawn(1)
 		multiplayer.peer_connected.connect(spawn)
 		multiplayer.peer_disconnected.connect(removePlayer)
+	#SignalBus.joinedLobby.connect(joinedLobby)
+	
+
+var players := {}
+
+#func joinedLobby():##Spawns player whenver you join a lobby
+	#if multiplayer.is_server():
+		#spawn(1)
+		#multiplayer.peer_connected.connect(spawn)
+		#multiplayer.peer_disconnected.connect(removePlayer)
 
 func spawnPlayer(data):
 	var p = playerScene.instantiate()
